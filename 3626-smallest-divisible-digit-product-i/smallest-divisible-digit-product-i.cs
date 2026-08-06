33e8fcef8dd77421fd924af8ec14f795ea8cd211
Harsh Sharma <hs96758737@gmail.com>
2026-08-06 11:24:55 +0530

    Time: 0 ms (100.00%) | Memory: 29 MB (86.67%) - LeetSync

---
+++ 3626-smallest-divisible-digit-product-i/smallest-divisible-digit-product-i.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int SmallestNumber(int n, int t) {
+        while (true) {
+            int product = 1;
+            int num = n;
+
+            if (num == 0)
+                product = 0;
+            else {
+                while (num > 0) {
+                    product *= (num % 10);
+                    num /= 10;
+                }
+            }
+
+            if (product % t == 0)
+                return n;
+
+            n++;
+        }
+    }
+}
