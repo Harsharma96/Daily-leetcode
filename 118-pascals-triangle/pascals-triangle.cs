87fc9d8e4d009aa24693f1c4180b3eefcd79a9c4
Harsh Sharma <hs96758737@gmail.com>
2026-08-21 12:43:23 +0530

    Time: 0 ms (100.00%) | Memory: 40.5 MB (24.36%) - LeetSync

---
+++ 118-pascals-triangle/pascals-triangle.cs
@@ -0,0 +1,28 @@
+public class Solution
+{
+    public IList<IList<int>> Generate(int numRows)
+    {
+        IList<IList<int>> result = new List<IList<int>>();
+
+        for (int i = 0; i < numRows; i++)
+        {
+            List<int> row = new List<int>();
+
+            for (int j = 0; j <= i; j++)
+            {
+                if (j == 0 || j == i)
+                {
+                    row.Add(1);
+                }
+                else
+                {
+                    row.Add(result[i - 1][j - 1] + result[i - 1][j]);
+                }
+            }
+
+            result.Add(row);
+        }
+
+        return result;
+    }
+}
