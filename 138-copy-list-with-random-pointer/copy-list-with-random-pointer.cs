af5c2ea8ff615cf03d370f955763a7170c960b07
Harsh Sharma <hs96758737@gmail.com>
2026-09-08 10:43:53 +0530

    Time: 82 ms (17.49%) | Memory: 42.3 MB (91.12%) - LeetSync

---
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cs
@@ -0,0 +1,46 @@
+/*
+// Definition for a Node.
+public class Node {
+    public int val;
+    public Node next;
+    public Node random;
+    
+    public Node(int _val) {
+        val = _val;
+        next = null;
+        random = null;
+    }
+}
+*/
+
+public class Solution {
+    public Node CopyRandomList(Node head) {
+        if (head == null)
+            return null;
+
+        Dictionary<Node, Node> map = new Dictionary<Node, Node>();
+
+        Node current = head;
+
+        while (current != null) {
+            map[current] = new Node(current.val);
+            current = current.next;
+        }
+
+        current = head;
+
+        while (current != null) {
+            map[current].next = current.next != null
+                ? map[current.next]
+                : null;
+
+            map[current].random = current.random != null
+                ? map[current.random]
+                : null;
+
+            current = current.next;
+        }
+
+        return map[head];
+    }
+}
