37adfac34243a67e111b8f7d1448db2ac7e61712
Harsh Sharma <hs96758737@gmail.com>
2026-09-20 09:11:03 +0530

    Time: 1 ms (100.00%) | Memory: 41.5 MB (87.09%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.cs
@@ -0,0 +1,12 @@
+public class Solution {
+    public string ReverseWords(string s) {
+        string[] words = s.Split(
+            new char[] { ' ' },
+            StringSplitOptions.RemoveEmptyEntries
+        );
+
+        Array.Reverse(words);
+
+        return string.Join(" ", words);
+    }
+}
